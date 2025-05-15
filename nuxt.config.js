eaa0062380d76fc12afe22dfc99b8de296bb9258
zamin6995 <akurai105@gmail.com>
2025-05-15 13:23:32 +0700

    Feature config base URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,7 +115,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://backend.test/api',
+    baseURL: 'http://truongnx_2023_03_07_backend.test/api',
   },
 
   router: {
